7f17085d1d38e73dbc8fad0472ee27b7c75d7f56
rockerle <16375280+rockerle@users.noreply.github.com>
2023-10-10 17:55:37 +0200

    ⠀⠀⠀⠀⠀⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⢿⣧⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⢸⣿⣇⠀⢸⣿⣿⣦⣤⣄⣀⣴⣿⣷⠀⠀⠀ ⠀⠀⠀⠀⠀⢸⣿⣿⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀ ⠀⠀⠀⠀⢀⣼⣿⣿⣧⣿⣿⣿⣿⡟⣿⣿⣿⠻⣿⠂⡀⠀ ⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣧⣿⣿⣿⣦⣿⣏⠁⠀ ⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀ ⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀ ⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀ ⢠⣾⣿⡿⠋⠀⠈⠙⣿⣿⣿⡿⣿⡿⠿⠟⢿⣿⣿⣷⣄⠀ ⠈⠿⡿⠃⠀⠀⠀⠀⣿⣿⣿⣧⠀⠀⠀⠀⠀⠉⠻⣿⡿⠂ ⠀⠀⠀⠀⠀⠀⠀⠈⢿⡿⠟⠃⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀

---
+++ src/main/java/rockerle/com/Cbpaster.java
@@ -0,0 +1,10 @@
+package rockerle.com;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cbpaster implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/rockerle/com/client/CbpasterClient.java
@@ -0,0 +1,34 @@
+package rockerle.com.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import java.util.List;
+import java.util.ListIterator;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class CbpasterClient implements ClientModInitializer {
+    KeyBinding print;
+    Keyboard clipboard;
+    List<String> content;
+    ListIterator<String> contentIter;
+    @Override
+    public void onInitializeClient() {
+
+        print = KeyBindingHelper.registerKeyBinding(new KeyBinding("print ASCII art", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_J,"printing ASCII art"));
+        clipboard = new Keyboard(MinecraftClient.getInstance());
+        ClientTickEvents.START_CLIENT_TICK.register(ctx->{
+            if(print.wasPressed()){
+                content = clipboard.getClipboard().lines().toList();
+                contentIter = content.listIterator();
+            }
+            if(content != null && contentIter.hasNext() && ctx.player.age%15==0)
+                ctx.getNetworkHandler().sendChatMessage(contentIter.next());
+        });
+    }
+}
