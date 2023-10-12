0214a9713665a52b5ceebf1f1f1f9183f0f0b58f
rockerle <16375280+rockerle@users.noreply.github.com>
2023-10-12 10:49:19 +0200

    __                      /\    .-" /                     /  ; .'  .'                    :   :/  .'                     \  ;-.'        .--""""--..__/     `.      .'           .'    `o  \     /                    `   ;    :                  \      :  .-;        -.         `.__.-' :  ;          \     ,   ; '._:           ;   :   (     \/  .__    ;    \   `-.  bug ;     "-,/_..--"`-..__)      '""--.._:

--- src/main/java/rockerle/com/client/CbpasterClient.java
+++ src/main/java/rockerle/com/client/CbpasterClient.java
@@ -1,12 +1,15 @@
 package rockerle.com.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.telemetry.WorldLoadedEvent;
 import org.lwjgl.glfw.GLFW;
 import java.util.List;
 import java.util.ListIterator;
@@ -22,6 +25,10 @@ public class CbpasterClient implements ClientModInitializer {
 
         print = KeyBindingHelper.registerKeyBinding(new KeyBinding("print ASCII art", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_J,"printing ASCII art"));
         clipboard = new Keyboard(MinecraftClient.getInstance());
+        ClientPlayConnectionEvents.DISCONNECT.register((handler,client)->{
+            content = null;
+            contentIter = null;
+        });
         ClientTickEvents.START_CLIENT_TICK.register(ctx->{
             if(print.wasPressed()){
                 content = clipboard.getClipboard().lines().toList();
