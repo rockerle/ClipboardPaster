e89299407ec1b8d6af837fb36b8fc71e94a9bf3b
rockerle <16375280+rockerle@users.noreply.github.com>
2024-08-13 11:20:12 +0200

    version bump to 1.21

--- src/main/java/rockerle/com/client/CbpasterClient.java
+++ src/main/java/rockerle/com/client/CbpasterClient.java
@@ -1,7 +1,6 @@
 package rockerle.com.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
@@ -9,7 +8,6 @@ import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.client.util.telemetry.WorldLoadedEvent;
 import org.lwjgl.glfw.GLFW;
 import java.util.List;
 import java.util.ListIterator;
